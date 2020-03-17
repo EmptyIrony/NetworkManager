46bc215b9936ae30bd321dd988850788ad05909a
EmptyIrony <1446188523@qq.com>
2020-03-17 21:19:43 +0800

    移除了好友系统

--- src/main/java/com/emptyirony/networkmanager/friend/Friend.java
+++ src/main/java/com/emptyirony/networkmanager/friend/Friend.java
@@ -1,8 +1,5 @@
 package com.emptyirony.networkmanager.friend;
 
-import com.emptyirony.networkmanager.friend.command.FriendCommand;
-import strafe.games.core.Stone;
-
 /**
  * 2 * @Author: EmptyIrony
  * 3 * @Date: 2020/2/25 15:57
@@ -12,7 +9,7 @@ public class Friend {
 
     public Friend() {
         System.out.println("初始化好友系统...");
-        Stone.get().getHoncho().registerCommand(new FriendCommand());
+//        Stone.get().getHoncho().registerCommand(new FriendCommand());
 
     }
 }
